f1ce45e7a12597882c9c7440b0225c5a31dc2258
Artyom Nogin <nogin98@gmail.com>
2022-10-29 14:15:45 +0600

    Little structure changes and beautify

--- differentqueries/select_order_by.sql
+++ differentqueries/select_order_by.sql
@@ -1,14 +1,20 @@
 select first_name, salary from employees order by salary;
+
 select * from employees where job_id = 'IT_PROG' order by salary;
+
 select first_name, salary, hire_date from employees where job_id = 'IT_PROG' order by hire_date desc;
+
 select last_name, salary, hire_date, hire_date+salary+2 expr
 from employees
 where employee_id>120
 order by expr;
+
 select * from employees order by commission_pct nulls first;
 
 select first_name, salary from employees order by 1;
+
 select * from employees order by 1 desc;
+
 select job_id, first_name, last_name, salary, hire_date
 from employees
 order by job_id desc, last_name, 4 desc;
--- differentqueries/select_quote.sql
+++ differentqueries/select_quote.sql
@@ -1,5 +1,9 @@
 select 'abc'||'xyz'||90*30 as result from dual;
+
 select * from dual;
+
 select 24*365*24*60*60 "moy vozrast v secundakh" from dual;
+
 select 'it''s my life' from dual;
+
 select q'<It's my life>' from dual;
