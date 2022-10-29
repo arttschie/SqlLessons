8fab26e208c4d6385b203ab0f0e6f1dff9f9ec8c
Artyom Nogin <nogin98@gmail.com>
2022-10-29 14:11:56 +0600

    Made beautify

--- homeworks/hw1.sql
+++ homeworks/hw1.sql
@@ -1,9 +1,17 @@
 select * from regions;
+
 select first_name, department_id, salary, last_name from employees;
+
 select employee_id, email, hire_date-7 "One week before hire date" from employees;
+
 select first_name||'('||job_id||')' our_emloyees from employees;
+
 select distinct first_name from employees;
+
 select job_title, 'min='||min_salary||', '|| 'max='||max_salary info, max_salary max, max_salary*2-2000 new_salary from jobs;
+
 select 'Peter''s dog is very clever' from dual;
+
 select q'<Peter's dog is very clever>' from dual;
+
 select 100*365.25*24*60 from dual;
